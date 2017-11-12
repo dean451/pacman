899e8ca801cf1e062769580415638001c8b6c93c
Dean Palm <dean@sxsw.com>
2017-11-12 16:15:32 -0600

    it works

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -1,20 +1,37 @@
-// in app/components/pac-man.js
-import Ember from 'ember';
-import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
-export default Ember.Component.extend(KeyboardShortcuts,{
-  didInsertElement: function() {
-    this.drawCircle();
-  },
+import Ember from "ember";
+import KeyboardShortcuts from "ember-keyboard-shortcuts/mixins/component";
 
-  drawCircle: function() {
-    let canvas = document.getElementById("myCanvas"); let ctx = canvas.getContext("2d");
-    let x = 50;
-    let y = 100;
-    let radius = 20;
-    ctx.fillStyle = '#000';
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-    ctx.closePath();
-    ctx.fill();
-  },
+export default Ember.Component.extend(KeyboardShortcuts, {
+    didInsertElement: function() {
+        this.drawCircle();
+    },
+
+    drawCircle: function() {
+        let canvas = document.getElementById("myCanvas");
+        let ctx = canvas.getContext("2d");
+        let x = 50;
+        let y = 200;
+        let radius = 30;
+
+        ctx.fillStyle = "#000";
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+        ctx.closePath();
+        ctx.fill();
+    },
+
+    keyboardShortcuts: {
+        up: function() {
+            console.log("up");
+        },
+        down: function() {
+            console.log("down");
+        },
+        left: function() {
+            console.log("left");
+        },
+        right: function() {
+            console.log("right");
+        },
+    }
 });
