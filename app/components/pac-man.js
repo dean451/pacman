a5f46993eba5fae3a9da46e7305a529616d5e8ce
Dean Palm <dean@sxsw.com>
2017-11-17 22:24:39 -0600

    computed screenwidth and height

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -9,6 +9,16 @@ export default Ember.Component.extend(KeyboardShortcuts, {
     y: 2,
     squareSize: 40,
 
+    screenPixelWidth: Ember.computed(function(){
+      return this.get('screenWidth') * this.get('squareSize');
+    }),
+    screenPixelHeight: Ember.computed(function(){
+      return this.get('screenHeight') * this.get('squareSize');
+    }),
+    // be sure to use these calculated values in 'clearscreen'
+
+
+
     ctx: Ember.computed(function(){
       let canvas = document.getElementById("myCanvas");
       let ctx = canvas.getContext("2d");
@@ -43,10 +53,7 @@ export default Ember.Component.extend(KeyboardShortcuts, {
     screenHeight: 15,
     clearScreen: function() {
       let ctx = this.get("ctx");
-      let screenPixelWidth = this.get('screenWidth') * this.get('squareSize');
-      let screenPixelHeight = this.get('screenHeight') * this.get('squareSize');
-
-      ctx.clearRect(0, 0, screenPixelWidth, screenPixelHeight)
+      ctx.clearRect(0, 0, this.get('screenPixelWidth'), this.get('screenPixelHeight'));
     },
 
     keyboardShortcuts: {
